116a6aa866c48b3edaf0a7dd9f9bb7db6ede879e
Smotrov Dmitriy <jinxx_develop@mail.ru>
2020-07-20 18:10:41 +0300

    Merge cells

--- xl.py
+++ xl.py
@@ -3,6 +3,7 @@
 import openpyxl as opx
 from openpyxl.styles.borders import Border, Side
 
+#need font-style type A
 
 workbook_path = "/home/jinxx/myWork/AutoDoc/test.xlsx"
 workbook = opx.load_workbook(workbook_path)
@@ -13,5 +14,12 @@ bold_border = Border(left=Side(style='thin'),
                      bottom=Side(style='thick'))
 
 ws = workbook.worksheets[0]
-ws.cell(row=3,column=2).border = bold_border
+ws.cell(row=2,column=2).border = bold_border
+ws.merge_cells('A2:D2')
+
+
+
+
+
+#close file with save
 workbook.save(workbook_path)
